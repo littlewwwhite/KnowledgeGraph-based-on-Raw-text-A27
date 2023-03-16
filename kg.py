6b895745f71901c7df19bb15b9b662aee18488a5
Wenjie Zhang <xerrors@163.com>
2023-03-17 00:07:23 +0800

    add stracture

--- kg.py
+++ kg.py
@@ -2,83 +2,50 @@ import os
 import json
 import random
 
-def check_input(prompt, keys):
-
-    checked = False
-    while not checked:
-        user_input = input(f"{prompt} > ")
-        if user_input in keys:
-            checked = True
-
-    return user_input
+from utils import refine_knowledge_graph
+from prepare.process import execute, gpuinit
 
 
-def refine_knowledge_graph(kg_path, refined_kg_path, fast_mode=False):
-    """用于手工对知识图谱进行筛选，也就是遍历 sentence 和 relations，然后手工筛选
+class ModelTrainer:
 
-    fast_mode: 如果为 True，直接写入
-    """
-
-    # 判断是否继续
-    try:
-        with open(refined_kg_path, 'r') as f:
-            start_pos = len(f.readlines())
-
-    except:
-        start_pos = 0
-
-    with open(kg_path, 'w') as f_in, open(refined_kg_path, 'a+') as f_out:
-        lines = f_in.readlines()
-        for line in lines[start_pos:]:
-            line = json.loads(f_in)
-
-            if fast_mode:
-                f_out.writelines(json.dumps(line, ensure_ascii=False) + "\n")
-                continue
-
-            refined_triples = []
-            print(line["sentText"])
-            for triple in line["relations"]:
-                print(f"主体：{triple['em1Text']}, 关系：{triple['label']}，客体：{triple['em2Text']}")
-                user_input = check_input("是否构成关系？是则直接【回车】，否则输入【Z】")
-                if not user_input:
-                    refined_triples.append(triple)
-
-            line["relations"] = refined_triples
-            f_out.writelines(json.dumps(line, ensure_ascii=False) + "\n")
-
-
-class ModelTrainer():
-
-    def __init__(self, data_path, generated_data_directory) -> None:
+    def __init__(self, data_path, output_dir) -> None:
         """ 用来训练 SPN 的一个类
 
         Args:
             data_path: 用于训练的所有数据，没有切分训练、测试、验证
-            generated_data_directory: 保存测试之后的数据的文件夹
+            output_dir: 保存测试之后的数据的文件夹
 
         """
         self.data_path = data_path
-        self.train_file = data_path.split(".")[0] + "_train.json"
-        self.valid_file = data_path.split(".")[0] + "_valid.json"
-        self.test_file = data_path.split(".")[0] + "_test.json"
+        self.generated_data_directory = output_dir
 
-        self.generated_data_directory = generated_data_directory
-        self.prediction = os.path.join(generated_data_directory, 'prediction.json') # 或者 pickle
-        self.test_result_format = os.path.join(generated_data_directory, 'test_resule_format.json')
-        self.test_result_refine = os.path.join(generated_data_directory, 'test_resule_refine.json')
+        os.makedirs(output_dir, exist_ok=True)
+        self.train_file = os.path.join(output_dir, "train.json")
+        self.valid_file = os.path.join(output_dir, "valid.json")
+        self.test_file = os.path.join(output_dir, "test.json")
 
-        self.params = "python -m main"
-        self.params += " --bert_directory BERT_PATH"
-        self.params += " --max_epoch 100"
-        self.params += " --max_span_length 10"
-        self.params += " --num_generated_triples 15"
-        self.params += " --max_grad_norm 2.5"
-        self.params += " --na_rel_coef 0.25"
-        self.params += f" --train_file {self.train_file}"
-        self.params += f" --valid_file {self.valid_file}"
-        self.params += f" --test_file {self.test_file}"
-        self.params += f" --generated_data_directory {self.generated_data_directory}"
+        self.prediction = os.path.join(output_dir, 'prediction.json') # 或者 .pickle
+        self.test_result_format = os.path.join(output_dir, 'test_result_format.json')
+        self.test_result_refine = os.path.join(output_dir, 'test_result_refine.json')
+
+        self.final_knowledge_graph = os.path.join(output_dir, 'knowledge_graph.json}')
+
+        self.split_data()
+        self.generate_running_cmd()
+
+    def generate_running_cmd(self):
+        params = "python -m main"
+        params += " --bert_directory BERT_PATH"
+        params += " --max_epoch 100"
+        params += " --max_span_length 10"
+        params += " --num_generated_triples 15"
+        params += " --max_grad_norm 2.5"
+        params += " --na_rel_coef 0.25"
+        params += f" --train_file {self.train_file}"
+        params += f" --valid_file {self.valid_file}"
+        params += f" --test_file {self.test_file}"
+        params += f" --generated_data_directory {self.generated_data_directory}"
+        return params
 
     def split_data(self):
         """将知识图谱数据(SPN_style)切分为三个文件"""
@@ -99,15 +66,10 @@ class ModelTrainer():
         """
 
         # TODO Code Here
-        #
-        #
-        #
-        #
 
     def train_and_test(self):
+        """训练并测试这个模型，测试的预测结果会保存到 self.prediction 这个文件里面。"""
         os.system(self.params)
-
-        return
 
     def relation_align(self):
         """读取预测的结果，并将结果跟 test_file 中的三元组对应上"""
@@ -127,29 +89,50 @@ class ModelTrainer():
         """将生成的 test_result_format 重新经过一遍人工清洗"""
         refine_knowledge_graph(self.test_result_format, self.test_result_refine)
 
-        # 然后跟 self.data_path 里面的 relations 合并，合并后保存到 kg_v1 里面
-
-        return "kg_v2 path"
+        # 然后跟 self.data_path 里面的 relations 合并，合并后保存到 self.final_knowledge_graph 里面
 
 
-class KG():
+        return self.final_knowledge_graph
+
+
+class KnowledgeGraphBuilder:
 
     def __init__(self) -> None:
-        self.data_dir = ""
-        self.text_path = ""
+        self.data_dir = "data/project_v1"  # 存放生成的数据的地方
+        self.text_path = "data/raw_data.txt" # 原始的文本文件
+        self.base_kg_path = os.path.join(self.data_dir, "base.json")
+        self.refined_kg_path = os.path.join(self.data_dir, "base_refined.json")
+
+        self.version = 0    # 会随着迭代次数的增加而增加
+
         self.kg_paths = [] # 一个数组，代表不同迭代版本的知识图谱
 
-        self.trainer = ModelTrainer()
+        gpu_init(0, 20000) # 初始化 GPU
 
 
+    def run_iteration(self):
+        print(f"Start Runing Iteration v{self.version}")
+
+        cur_data_path = self.kg_path[-1]
+        cur_out_path = os.path.join(self.data_dir, f"iteration_v{self.version}")
+        triner = ModelTrainer(cur_data_path, cur_out_path)
+
+        # 判断是否已经训练过了，毕竟这个地方可能会出问题的
+        if not os.path.exists(trainer.prediction):
+            trainer.train_and_test()
+
+        trainer.relation_align()
+        trainer.refine_and_extend()
+        self.version += 1
+
+        self.kg_paths.append(trainer.final_knowledge_graph)
 
 
-    def get_base_kg_from_txt(self, text_path, base_res_path):
+    def get_base_kg_from_txt(self):
         """ Get base knowledge graph by UIE and format it to SPN style
 
-        Args:
-            text_path: the raw sentence path.
-            json_path: the formatted data.
+        input: self.text_path
+        output: self.base_kg_path
         """
 
         # 将 extract.py 里面的内容在这里面调用，这里面可能也是包含了多个函数的调用的
@@ -160,12 +143,34 @@ class KG():
         # 1. 清洗文本
 
         # 2. 切分句子
+        texts = []
 
-        # 3. 喂给 UIE 并得到 relations，注意这里要保存句子的 id（从 0 开始算）
+        # 3. 喂给 UIE 并得到 relations，注意这里要保存句子的 id（从 0 开始算
+        #    注意：这里如果发现已经存在了 self.base_kg_path，就跳过 UIE
+        #    如果想要重新使用 UIE 抽取，删掉这个文件就行
+        if not os.path.exists(self.base_kg_path):
+            all_items = execute(texts)
+            with open(self.base_kg_path, 'w') as f:
+                for item in all_items():
+                    f.writelines(json.dumps(item, ensure_ascii=False) + "\n")
 
         # 4. 算法验证，使用 bertTokenizer 检测一下实体是否还存在于句子里面
+        filtted_items = auto_filter(all_items)
 
-        # 5. 保存到指定文件 base_res_path
+        # 5. 人工筛选并保存，因为需要加断点，所以需要一边做一边保存
+        manual_filter(filtted_items, self.refined_kg_path)
 
+    def save_to_local(self):
+        """用于将这个类保存到本地的一个方法"""
         pass
 
+    def load_from_local(self, path):
+        pass
+
+
+if __name__ == "__main__":
+
+    KG = KnowledgeGraphBuilder()
+
+    # startup
+    KG.get_base_kg_from_txt()  # 预计用时：
---
+++ prepare/process.py
@@ -0,0 +1,94 @@
+import os, json
+from prepare.schema import schema_v2
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '3'
+import paddle.inference as paddle_infer
+from paddlenlp import Taskflow
+
+# 定义一个函数，用于关系抽取
+def paddle_relation_ie(content):
+    relation_ie = Taskflow("information_extraction", schema=schema_v2.schema, batch_size=1)
+    return relation_ie(content)
+
+# 定义一个函数，用于初始化GPU
+def gpu_init(gpu_id, gpu_mem, ):
+    config = paddle_infer.Config("./mobilenet.pdmodel", "./mobilenet.pdiparams")
+    config.enable_use_gpu(gpu_mem, gpu_id)
+    print("Use GPU is: {}".format(config.use_gpu())) # True
+    print("Init mem size is: {}".format(config.memory_pool_init_size_mb())) # 100
+    print("Init mem frac is: {}".format(config.fraction_of_gpu_memory_for_pool())) # 0.003
+    print("GPU device id is: {}".format(config.gpu_device_id())) # 0
+
+# def read_txt_file(path):
+#     with open(path, 'r') as f:
+#         while True:
+#             line = f.readline()
+#             if not line:
+#                 break
+#             yield line.strip()
+
+# 关系抽取并修改json文件
+def rel_json(content):
+    all_relations = [] # 定义一个空列表，用于存储每个chapter的关系信息
+    res_relation = paddle_relation_ie(content)  # 传入文本进行关系识别
+    for rel in res_relation:
+        for sub_type, sub_rel in rel.items():
+            for sub in sub_rel:
+                if sub.get("relations") is None:
+                    continue
+                for rel_type, rel_obj in sub["relations"].items():
+                    for obj in rel_obj:
+                        if not sub['text'] or not obj['text']:
+                            continue
+                        rel_triple = {"em1Text": sub['text'],"em2Text": obj['text'],"label": rel_type}
+                        all_relations.append(rel_triple)
+    return all_relations
+
+
+# # 将实体和关系信息写入json文件
+# def write_json(all_relations, write_dir, content, sent_id):
+#     dict= {}
+#     dict["id"] = sent_id
+#     dict["sentText"] = content
+#     dict["relationMentions"] = all_relations
+#     with open(write_dir + ".json", "a", encoding="utf-8") as f:
+#         json.dump(dict, f, ensure_ascii=False, separators=(',', ':'))
+#         f.write('\n')
+
+# 执行函数
+def execute(texts):
+
+    sent_id = 0
+    all_items = []
+    for line in texts:
+        line = line.strip()
+        all_relations = rel_json(line)
+
+        item = {}
+        item["id"] = sent_id
+        item["sentText"] = content
+        item["relationMentions"] = all_relations
+
+        sent_id += 1
+        if sent_id % 10 == 0 and sent_id != 0:
+            print("Done {} lines".format(flag))
+
+        all_items.append(item)
+
+    return all_items
+    # with open(write_dir, 'w') as f:
+    #     for item in all_items():
+    #         f.writelines(json.dumps(item, ensure_ascii=False) + "\n")
+
+
+def main():
+    gpu_init(0, 20000)
+    version = 'res_base_v2'
+    data_dir = '/data_F/zhijian/paddlenlp/data/'
+    txt_path = os.path.join(data_dir, 'raw_data/raw_data_lines.txt')
+    write_dir = os.path.join(data_dir, 'res_data/', version)
+    execute(txt_path, write_dir)
+
+
+if __name__ == '__main__':
+    main()
