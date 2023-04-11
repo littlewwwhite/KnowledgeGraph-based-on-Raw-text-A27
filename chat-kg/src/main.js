88b6f19a75d1f49e6fa7dbd6d280aad140c3e238
Xerrors <xerrors@163.com>
2023-04-11 20:09:42 +0800

    web update

--- chat-kg/src/main.js
+++ chat-kg/src/main.js
@@ -4,11 +4,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Antd)
 
 app.mount('#app')
--- chat-kg/src/views/ChatView.vue
+++ chat-kg/src/views/ChatView.vue
@@ -1,40 +1,85 @@
 <template>
-  <div class="chat">
-    <div ref="chatBox" class="chat-box">
-      <div
-        v-for="message in state.messages"
-        :key="message.id"
-        class="message-box"
-        :class="message.type"
-      >
-        <img v-if="message.filetype === 'image'" :src="message.url" class="message-image" alt="">
-        <p v-else style="white-space: pre-line" class="message-text">{{ message.text }}</p>
+  <div class="chat-container">
+    <div class="chat">
+      <div ref="chatBox" class="chat-box">
+        <div
+          v-for="message in state.messages"
+          :key="message.id"
+          class="message-box"
+          :class="message.type"
+        >
+          <img v-if="message.filetype === 'image'" :src="message.url" class="message-image" alt="">
+          <p v-else style="white-space: pre-line" class="message-text">{{ message.text }}</p>
+        </div>
+      </div>
+      <div class="input-box">
+        <a-button size="large" @click="clearChat">
+          <template #icon> <ClearOutlined /> </template>
+        </a-button>
+        <a-input
+          type="text"
+          class="user-input"
+          v-model:value="state.inputText"
+          @keydown.enter="sendMessage"
+          placeholder="输入问题……"
+        />
+        <a-button size="large" @click="sendMessage" :disabled="!state.inputText">
+          <template #icon> <SendOutlined /> </template>
+        </a-button>
       </div>
     </div>
-    <div class="input-box">
-      <button @click="clearChat"><clear-outlined /></button>
-      <input
-        type="text"
-        class="user-input"
-        v-model="state.inputText"
-        @keydown.enter="sendMessage"
-        placeholder="输入问题……"
-      />
-      <button @click="sendMessage" :disabled="!state.inputText"><send-outlined /></button>
+    <div class="info">
+      <h1>{{ info.title }}</h1>
+      <p class="description" >{{ info.description }}</p>
+      <a-image v-if="info.image" :src="info.image" :alt="info.title"/>
+      <!-- <p>三元组数量：{{ info.triples.length }}</p>
+      <p>句子数量：{{ info.sents.length }}</p> -->
+      <div id="lite_graph" v-show="info.graph?.nodes.length > 0"></div>
+      <a-collapse v-model:activeKey="state.activeKey" v-if="info.graph?.sents.length > 0" accordion>
+        <a-collapse-panel
+          v-for="(sent, index) in info.graph.sents"
+          :key="index"
+          :header="'相关句子 ' + (index + 1)"
+          :show-arrow="false"
+          ghost
+        >
+          <p>{{ sent }}</p>
+        </a-collapse-panel>
+      </a-collapse>
     </div>
   </div>
 </template>
 
 <script setup>
+import * as echarts from 'echarts';
 import { reactive, ref, onMounted } from 'vue'
 import { SendOutlined, ClearOutlined } from '@ant-design/icons-vue'
 
+let myChart = null;
 const chatBox = ref(null)
 const state = reactive({
   history: [],
   messages: [],
+  activeKey: [],
   inputText: ''
 })
+
+const default_info = {
+  title: '知识图谱问答 ChatKG',
+  description: '由江南大学人工智能与计算机学院开发的基于知识图谱的问答系统，支持多轮对话，支持外部信息检索',
+  image: 'https://news.jiangnan.edu.cn/__local/6/D9/98/C642DE3CDC7F72EC01C8A84FEE8_39252AC8_1D99C.jpg',
+  graph: null,
+}
+
+const info = reactive({
+  ...default_info
+})
+
+const scrollToBottom = () => {
+  setTimeout(() => {
+    chatBox.value.scrollTop = chatBox.value.scrollHeight - chatBox.value.clientHeight
+  }, 10) // 10ms 后滚动到底部
+}
 
 const appendMessage = (message, type) => {
   state.messages.push({
@@ -42,10 +87,7 @@ const appendMessage = (message, type) => {
     type,
     text: message
   })
-  // 滚动到底部
-  setTimeout(() => {
-    chatBox.value.scrollTop = chatBox.value.scrollHeight - chatBox.value.clientHeight
-  }, 10)
+  scrollToBottom()
 }
 
 
@@ -56,10 +98,7 @@ const appendPicMessage = (pic, type) => {
     filetype: "image",
     url: pic
   })
-  // 滚动到底部
-  setTimeout(() => {
-    chatBox.value.scrollTop = chatBox.value.scrollHeight - chatBox.value.clientHeight
-  }, 10)
+  scrollToBottom()
 }
 
 const updateLastReceivedMessage = (message, id) => {
@@ -73,10 +112,7 @@ const updateLastReceivedMessage = (message, id) => {
       text: message
     })
   }
-  // 滚动到底部
-  setTimeout(() => {
-    chatBox.value.scrollTop = chatBox.value.scrollHeight - chatBox.value.clientHeight
-  }, 10)
+  scrollToBottom()
 }
 
 const sendMessage = () => {
@@ -99,15 +135,20 @@ const sendMessage = () => {
       const reader = response.body.getReader()
       const decoder = new TextDecoder()
       let buffer = ''
-      let pic;
+      let pic
+      let wiki
+      let graph
       // 逐步读取响应文本
       const readChunk = () => {
         return reader.read().then(({ done, value }) => {
           if (done) {
-            if (pic) {
-              setTimeout(() => {
-                appendPicMessage(pic, 'received')
-              }, 500);
+            info.image = pic
+            info.graph = graph
+            // 处理维基百科的内容
+            info.title = wiki?.title
+            info.description = wiki?.summary
+            if (info.graph) {
+              myChart.setOption(graphOption(info.graph));
             }
             console.log('Finished')
             return
@@ -122,6 +163,8 @@ const sendMessage = () => {
             updateLastReceivedMessage(data.updates.response, cur_res_id)
             state.history = data.history
             pic = data.image
+            wiki = data.wiki
+            graph = data.graph
             buffer = ''
           } catch (e) {
             console.log(e)
@@ -130,8 +173,6 @@ const sendMessage = () => {
           return readChunk()
         })
       }
-
-
       return readChunk()
     })
   } else {
@@ -139,23 +180,94 @@ const sendMessage = () => {
   }
 }
 
+const graphOption = (graph) => {
+  console.log(graph)
+  graph.nodes.forEach(node => {
+    node.symbolSize = 5;
+    node.label = {
+      show: true
+    }
+  });
+  let option = {
+    tooltip: {
+      show: true, //默认值为true
+      showContent: true, //是否显示提示框浮层
+      trigger: 'item', //触发类型，默认数据项触发
+      triggerOn: 'mousemove', //提示触发条件，mousemove鼠标移至触发，还有click点击触发
+      alwaysShowContent: false, //默认离开提示框区域隐藏，true为一直显示
+      showDelay: 0, //浮层显示的延迟，单位为 ms，默认没有延迟，也不建议设置。在 triggerOn 为 'mousemove' 时有效。
+      hideDelay: 200, //浮层隐藏的延迟，单位为 ms，在 alwaysShowContent 为 true 的时候无效。
+      enterable: false, //鼠标是否可进入提示框浮层中，默认为false，如需详情内交互，如添加链接，按钮，可设置为 true。
+      position: 'right', //提示框浮层的位置，默认不设置时位置会跟随鼠标的位置。只在 trigger 为'item'的时候有效。
+      confine: false, //是否将 tooltip 框限制在图表的区域内。外层的 dom 被设置为 'overflow: hidden'，或者移动端窄屏，导致 tooltip 超出外界被截断时，此配置比较有用。
+      // transitionDuration: 0.1, //提示框浮层的移动动画过渡时间，单位是 s，设置为 0 的时候会紧跟着鼠标移动。
+      formatter: (x) => x.data.name
+    },
+    series: [
+      {
+        type: 'graph',
+        draggable: true,
+        layout: 'force',
+        data: graph.nodes.map(function (node, idx) {
+          node.id = idx;
+          return node;
+        }),
+        links: graph.links,
+        categories: graph.categories,
+        roam: true,
+        label: {
+          position: 'right'
+        },
+        force: {
+          repulsion: 100
+        },
+        lineStyle: {
+          color: 'source',
+          curveness: 0.1
+        },
+      }
+    ]
+  };
+
+  return option
+}
+
+
+const sendDeafultMessage = () => {
+  setTimeout(() => {
+    appendMessage('你好？我是 ChatKG，有什么可以帮你？', 'received')
+  }, 1000);
+}
+
 const clearChat = () => {
   state.messages = []
   state.history = []
+  info.title = default_info.title
+  info.description = default_info.description
+  info.image = default_info.image
+  info.graph = default_info.graph
+  info.sents = default_info.sents
+  sendDeafultMessage()
 }
 
 onMounted(() => {
-  state.inputText = '你好'
-  setTimeout(() => {
-    sendMessage()
-  }, 100);
+  sendDeafultMessage()
+  myChart = echarts.init(document.getElementById('lite_graph'));
+
 })
 </script>
 
 <style lang="less" scoped>
+.chat-container {
+  display: flex;
+  gap: 1.5rem;
+}
+
 .chat {
   display: flex;
+  width: 100%;
   max-width: 800px;
+  flex-grow: 1;
   margin: 0 auto;
   flex-direction: column;
   height: calc(100vh - 200px);
@@ -216,6 +328,7 @@ onMounted(() => {
 
 p.message-text {
   word-wrap: break-word;
+  margin-bottom: 0;
 }
 
 img.message-image {
@@ -231,10 +344,10 @@ img.message-image {
   border-top: 1px solid #ccc;
 }
 
-/* 编写一个好看的输入框，小圆角，白色背景，柔和的阴影 */
 input.user-input {
   flex: 1;
-  padding: 0.8rem 1rem;
+  height: 40px;
+  padding: 0.5rem 1rem;
   background-color: #fff;
   border: none;
   border-radius: 8px;
@@ -243,30 +356,72 @@ input.user-input {
   margin: 0 1rem;
   color: #111111;
   font-size: 16px;
-  line-height: 22px;
+  // line-height: 22px;
   font-variation-settings: 'wght' 400, 'opsz' 10.5;
 }
 
-input:focus {
-  outline: none;
-  box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.1),
-    0 0 0 3px rgba(0, 0, 0, 0.1);
-}
-
-button {
-  padding: 0.8rem 1rem;
-  // background: linear-gradient(90deg, #006880 10.79%, #005366 87.08%);
-  background: linear-gradient(90deg, #555 10.79%, #333 87.08%);
-  // background-color: #333;
-  color: #fff;
-  border: none;
+.ant-btn-icon-only {
+  font-size: 16px;
   border-radius: 8px;
   cursor: pointer;
-  font-size: 1rem;
 }
 
-button:disabled {
-  // background: #ccc;
-  cursor: not-allowed;
+// button:disabled {
+//   // background: #ccc;
+//   cursor: not-allowed;
+// }
+
+div.info {
+  width: 400px;
+  min-width: 400px;
+  height: calc(100vh - 200px);
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 0;
+
+  // 平滑滚动
+  scroll-behavior: smooth;
+
+  &::-webkit-scrollbar {
+    width: 0rem;
+  }
+  // background-color: #ccc;
+  // margin: 0 1rem;
+
+  & > h1 {
+    font-size: 1.5rem;
+    margin: 0.5rem 0;
+    // padding: 0.5rem;
+    // text-align: center;
+  }
+
+  & > p.description {
+    font-size: 1rem;
+    margin: 0;
+    // padding: 0.5rem;
+    // max-height: 10rem;
+    margin-bottom: 20px;
+    // text-align: center;
+  }
+
+  .ant-image {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    border-radius: 8px;
+    overflow: hidden;
+    overflow-y: hidden
+  }
+
+  #lite_graph {
+    width: 400px;
+    height: 300px;
+    background: #f2f2f2;
+    // border: 4px solid #ccc;
+    border-radius: 8px;
+    box-shadow: 0px 0.3px 0.9px rgba(0, 0, 0, 0.12), 0px 0.6px 2.3px rgba(0, 0, 0, 0.1),
+      0px 1px 5px rgba(0, 0, 0, 0.08);
+  }
 }
 </style>
