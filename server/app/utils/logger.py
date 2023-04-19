11c03caf21a898c46c272e71bcac3016dd42c2e4
PatrickStar <34784086+littlewwwhite@users.noreply.github.com>
2023-04-19 18:11:32 +0800

    学习

--- server/app/utils/logger.py
+++ server/app/utils/logger.py
@@ -3,7 +3,7 @@ import logging
 import logging.config
 import os
 
-FORMAT = '%(asctime)s %(levelname)s %(name)s %(message)s'
-logging.basicConfig(format=FORMAT)
-logger = logging.getLogger('server')
+FORMAT = '%(asctime)s %(levelname)s %(name)s %(message)s' # 配置日志格式
+logging.basicConfig(format=FORMAT)# basicConfig函数对日志的输出格式及方式做相关配置
+logger = logging.getLogger('server')# getLogger函数通过指定的名称获取日志器
 
